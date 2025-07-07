edad93a8f9b5b813fb13ec94aebdedd7ad017f33
Shiva-04z <22it10sh247@mitsgwl.ac.in>
2025-07-07 11:42:02 +0530

    Sending for IOS build

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Apple Grower',
+      title: 'Book My Load',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         cardColor: Colors.white,
